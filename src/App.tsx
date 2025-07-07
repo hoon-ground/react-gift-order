cdb494f7878e412cddd787fd1233c8f036409de5
hoon_ground <break_the_limit@naver.com>
2025-07-07 14:18:07 +0900

    feat : Context API/sessionStorage를 이용한 전역 user정보 저장 및 유지

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import MainPage from './pages/MainPage'
-import LoginPage from './pages/LoginPage'
-import NotFoundPage from './pages/NotFoundPage'
+import MainPage from '@/pages/MainPage'
+import LoginPage from '@/pages/LoginPage'
+import NotFoundPage from '@/pages/NotFoundPage'
 import Layout from '@/components/Layout'
+import { UserProvider } from '@/contexts/UserContext'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
+      <UserProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </UserProvider>
     </BrowserRouter>
   )
 }
---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+
+interface User {
+  email: string
+}
+
+interface UserContextType {
+  user: User | null
+  login: (user: User) => void
+  logout: () => void
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined)
+
+export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null)
+
+  useEffect(() => {
+    const storedUser = sessionStorage.getItem('user')
+    if (storedUser) {
+      setUser(JSON.parse(storedUser))
+    }
+  }, [])
+
+  const login = (user: User) => {
+    sessionStorage.setItem('user', JSON.stringify(user))
+    setUser(user)
+  }
+
+  const logout = () => {
+    sessionStorage.removeItem('user')
+    setUser(null)
+  }
+
+  return (
+    <UserContext.Provider value={{ user, login, logout }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export const useUser = () => {
+  const context = useContext(UserContext)
+  if (!context) throw new Error('UserProvider로 감싼 컴포넌트 안에서 사용해야 합니다~')
+  return context
+}
